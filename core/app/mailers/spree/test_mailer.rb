e6f37fbbadd18d526bac852468049901f487a4da
Niklas Schreiber <niklas@derschreiber.com>
2014-12-09 12:54:46 +0100

    fix email subject

--- core/app/mailers/spree/test_mailer.rb
+++ core/app/mailers/spree/test_mailer.rb
@@ -1,7 +1,7 @@
 module Spree
   class TestMailer < BaseMailer
     def test_email(email)
-      subject = "#{Spree::Store.current.name} #{Spree.t('test_mailer.test_email.subject')}"
+      subject = "#{Spree::Config[:site_name]} #{Spree.t('test_mailer.test_email.subject')}"
       mail(to: email, from: from_address, subject: subject)
     end
   end
